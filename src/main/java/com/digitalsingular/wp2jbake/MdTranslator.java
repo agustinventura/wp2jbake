f84035ace16d435a23a7ca06cada0ce1b0bf3c2b
Agustin Ventura <agustinventura@gmail.com>
2016-06-09 12:13:19 +0200

    Added captions and headings translations.

--- src/main/java/com/digitalsingular/wp2jbake/MdTranslator.java
+++ src/main/java/com/digitalsingular/wp2jbake/MdTranslator.java
@@ -1,6 +1,8 @@
 package com.digitalsingular.wp2jbake;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
@@ -12,10 +14,21 @@ public class MdTranslator {
     public static final String POST_DATE_FORMAT = "yyyy-MM-dd";
     public static final String TAG_DELIMITER = ",";
     public static final String START_PRE = "<pre[^>]*>";
-    public static final String START_PRETTYPRINT = "\n```prettyprint\n";
+    public static final String START_PRETTYPRINT = "\n```prettyprint linenums\n";
     public static final String END_PRE = "</pre>";
     public static final String END_PRETTYPRINT = "\n```\n";
     public static final String START_H1 = "<h1>";
+    public static final String START_H5 = "\n<h5>";
+    public static final String END_H1 = "</h1>";
+    public static final String END_H5 = "</h5>\n";
+    public static final String START_H2 = "<h2>";
+    public static final String START_H6 = "\n<h6>";
+    public static final String END_H2 = "</h2>";
+    public static final String END_H6 = "</h6>\n";
+    public static final String START_H3 = "<h3>";
+    public static final String END_H3 = "</h3>";
+    public static final String START_WORDPRESS_CAPTION = "\\[caption[^\\]]*\\]";
+    public static final String END_WORDPRESS_CAPTION = "\\[/caption\\]";
 
 
     public String translateDate(Date dateToTranslate) {
@@ -30,7 +43,20 @@ public class MdTranslator {
     public String translateContent(String contentToTranslate) {
         String translatedContent = translatePre(contentToTranslate);
         translatedContent = translateHeadings(translatedContent);
+        translatedContent = deleteWordpressImages(translatedContent);
+        translatedContent = addNewLinesToBlockElements(translatedContent);
         return translatedContent;
+    }
+
+    private String addNewLinesToBlockElements(String translatedContent) {
+        String blockElementStart = "<pre[^>]*>";
+        return translatedContent;
+    }
+
+    private String deleteWordpressImages(String contentToTranslate) {
+        String contentWithoutStartingTag = contentToTranslate.replaceAll(START_WORDPRESS_CAPTION, StringUtils.EMPTY);
+        String contentWithoutEndingTag = contentWithoutStartingTag.replaceAll(END_WORDPRESS_CAPTION, StringUtils.EMPTY);
+        return contentWithoutEndingTag;
     }
 
     private String translatePre(String contentToTranslate) {
@@ -40,12 +66,12 @@ public class MdTranslator {
     }
 
     private String translateHeadings(String contentToTranslate) {
-        String contentWithoutStartingh1 = contentToTranslate.replace(START_H1, "<h5>");
-        String contentWithoutEndingh1 = contentWithoutStartingh1.replace("</h1>", "</h5>");
-        String contentWithoutStartingh2 = contentWithoutEndingh1.replace("<h2>", "<h6>");
-        String contentWithoutEndingh2 = contentWithoutStartingh2.replace("</h2>", "</h6>");
-        String contentWithoutStartingh3 = contentWithoutEndingh2.replace("<h3>", "<h7>");
-        String contentWithoutEndingh3 = contentWithoutStartingh3.replace("</h3>", "</h7>");
+        String contentWithoutStartingh1 = contentToTranslate.replace(START_H1, START_H5).replace(START_H1.toUpperCase(), START_H5);
+        String contentWithoutEndingh1 = contentWithoutStartingh1.replace(END_H1, END_H5).replace(END_H1.toUpperCase(), END_H5);
+        String contentWithoutStartingh2 = contentWithoutEndingh1.replace(START_H2, START_H6).replace(START_H2.toUpperCase(), START_H6);
+        String contentWithoutEndingh2 = contentWithoutStartingh2.replace(END_H2, END_H6).replace(END_H2.toUpperCase(), END_H6);
+        String contentWithoutStartingh3 = contentWithoutEndingh2.replace(START_H3, START_H6).replace(START_H3.toUpperCase(), START_H6);
+        String contentWithoutEndingh3 = contentWithoutStartingh3.replace(END_H3, END_H6).replace(END_H3.toUpperCase(), END_H6);
         return contentWithoutEndingh3;
     }
 }
--- src/test/java/com/digitalsingular/wp2jbake/MdTranslatorTest.java
+++ src/test/java/com/digitalsingular/wp2jbake/MdTranslatorTest.java
@@ -16,10 +16,12 @@ public class MdTranslatorTest {
     public static final String TAG_2 = "tag 2";
     public static final String TEST_DATE = "2016-01-01";
     public static final String TEST_CONTENT = "content";
-    public static final String TEST_PRE_CONTENT = "<pre lang=\"java\"> contenido java </pre>";
-    public static final String TEST_PRE_RESULT = "\n```prettyprint\n contenido java \n```\n";
-    private static final String TEST_HEADING_CONTENT = "<h1> Titulo </h1>";
-    private static final String TEST_HEADING_RESULT = "<h5> Titulo </h5>";
+    public static final String TEST_PRE_CONTENT = "<pre lang=\"java\"> java content </pre>";
+    public static final String TEST_PRE_RESULT = "\n```prettyprint linenums\n java content \n```\n";
+    public static final String TEST_HEADING_CONTENT = "<h1> Title </h1><H2> Subtitle </H2>";
+    public static final String TEST_HEADING_RESULT = "\n<h5> Title </h5>\n\n<h6> Subtitle </h6>\n";
+    public static final String TEST_WORDPRESS_TAGS_CONTENT = "[caption id='image'] image [/caption]";
+    public static final String TEST_WORDPRESS_TAGS_RESULT = " image ";
 
     private MdTranslator sut = new MdTranslator();
 
@@ -57,5 +59,17 @@ public class MdTranslatorTest {
     public void translateContentWithHeading() {
         String translatedContent = sut.translateContent(TEST_HEADING_CONTENT);
         assertThat(translatedContent, is(TEST_HEADING_RESULT));
+    }
+
+    @Test
+    public void translateContentWithWordpressTags() {
+        String translatedContent = sut.translateContent(TEST_WORDPRESS_TAGS_CONTENT);
+        assertThat(translatedContent, is(TEST_WORDPRESS_TAGS_RESULT));
+    }
+
+    @Test
+    public void translateContentWithBlockElements() {
+        String translatedContent = sut.translateContent("<ul><li></li></ul><div><p></p></div>");
+        assertThat(translatedContent, is("\n<ul><li></li></ul>\n\n<div>\n<p></p>\n</div>\n"));
     }
 }
